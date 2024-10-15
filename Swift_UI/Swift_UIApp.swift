64ca93e2204349f41a8e533aa4d2ba527e229b16
MakiRoyal <mika.ramanantsoa@epitech.digital>
2024-10-15 16:12:01 +0200

    Initial Commit

---
+++ Swift_UI/Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UIApp.swift
+//  Swift_UI
+//
+//  Created by Mika Ramanantsoa on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
